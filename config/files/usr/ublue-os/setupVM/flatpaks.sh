b386ee99dcd9cb6071ddae7d02ad93149a99cf21
kkb301 <138193877+kkb301@users.noreply.github.com>
2024-07-23 06:48:22 +0100

    startup

---
+++ config/files/usr/ublue-os/setupVM/flatpaks.sh
@@ -0,0 +1,5 @@
+flatpak remote-add --if-not-exists --system flathub https://flathub.org/repo/flathub.flatpakrepo
+flatpak remote-delete fedora
+flatpak install -y org.keepassxc.KeePassXC
+flatpak install -y nz.mega.MEGAsync
+flatpak install -y org.geany.Geany
