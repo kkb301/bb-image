0b570c43e880f7e982aac7eaac76f59bac4b07ef
kevin <kevin@laptop.pivpn>
2024-11-28 20:05:53 +0000

    update files

--- files/gnome/kevin/flatpaks.sh
+++ files/gnome/kevin/flatpaks.sh
@@ -1,9 +1,34 @@
-
-flatpak uninstall --all 
-flatpak remote-add --if-not-exists --system flathub https://flathub.org/repo/flathub.flatpakrepo
-flatpak remote-delete fedora
-
-
-
-flatpak install -y org.geany.Geany
+flatpak install -y com.brave.Browser
+flatpak install -y com.github.tchx84.Flatseal
+flatpak install -y com.google.Chrome
+flatpak install -y com.google.EarthPro
+flatpak install -y com.mattjakeman.ExtensionManager
+flatpak install -y com.rawtherapee.RawTherapee
+flatpak install -y io.github.dvlv.boxbuddyrs
+flatpak install -y io.github.shiftey.Desktop
+flatpak install -y io.github.ungoogled_software.ungoogled_chromium
 flatpak install -y io.github.vikdevelop.SaveDesktop
+flatpak install -y io.gitlab.librewolf-community
+flatpak install -y io.podman_desktop.PodmanDesktop
+flatpak install -y net.giuspen.cherrytree
+flatpak install -y net.mullvad.MullvadBrowser
+flatpak install -y org.adishatz.Screenshot
+flatpak install -y org.cockpit_project.CockpitClient
+flatpak install -y org.darktable.Darktable
+flatpak install -y org.fedoraproject.MediaWriter
+flatpak install -y org.filezillaproject.Filezilla
+flatpak install -y org.freac.freac
+flatpak install -y org.freefilesync.FreeFileSync
+flatpak install -y org.geany.Geany
+flatpak install -y org.gimp.GIMP
+flatpak install -y org.gnome.Calendar
+flatpak install -y org.gnome.Cheese
+flatpak install -y org.gnome.Epiphany
+flatpak install -y org.gnome.Logs
+flatpak install -y org.gnome.Shotwell
+flatpak install -y org.gnome.seahorse.Application
+flatpak install -y org.keepassxc.KeePassXC
+flatpak install -y org.libreoffice.LibreOffice
+flatpak install -y org.onlyoffice.desktopeditors
+flatpak install -y org.strawberrymusicplayer.strawberry
+flatpak install -y org.videolan.VLC
